a69f38f4b1c1cf00bf80b1834d222ef0a29c3049
Néstor Sancho <theNestruo@users.noreply.github.com>
2020-12-07 14:09:29 +0100

    PNG2MSX initial version

--- makefile
+++ makefile
@@ -4,12 +4,30 @@
 
 CCOMPILER=gcc
 CCOMPILER_OPTIONS=-Wall -O3
-PCX2MSX=pcx2msx
-PCX2SPR=pcx2spr
-PCX2MSXPLUS=pcx2msx+
-PCX2SPRPLUS=pcx2spr+
-PNG2MSX=png2msx
-TMX2BIN=tmx2bin
+PCX2MSX=bin/pcx2msx
+PCX2SPR=bin/pcx2spr
+PCX2MSXPLUS=bin/pcx2msx+
+PCX2SPRPLUS=bin/pcx2spr+
+PNG2MSX=bin/png2msx
+TMX2BIN=bin/tmx2bin
 REMOVE=rm
+
+#
+# file lists
+#
+
+TESTS=\
+	test/charset.pcx.chr \
+	test/charset.pcx.clr \
+	test/charset.png.chr \
+	test/charset.png.clr \
+	test/bigsprites.pcx.spat.asm \
+	test/bigsprites.pcx.spr.asm \
+	test/screen.tmx.bin \
+	test/metatiles.tmx.bin
+
+#
+# main makefile
+#
 
 include makefile.common
--- src/readpng.c
+++ src/readpng.c
@@ -42,37 +42,37 @@ struct stColor {
 /* Constant values --------------------------------------------------------- */
 
 struct stColor palette[] = {
-		{ 0x00, 0x00, 0x00, 0xff,  0 }, // actually transparent
-		{ 0x40, 0x40, 0x40, 0x00,  0 }, // dark grey as transparent
+		{ 0x00, 0x00, 0x00, 0xff,  1 }, // black		TMS9918	V9938
+		{ 0x23, 0xcb, 0x32, 0xff,  2 }, // medium green	TMS9918
+		{ 0x24, 0xda, 0x24, 0xff,  2 }, // medium green			V9938
+		{ 0x60, 0xdd, 0x6c, 0xff,  3 }, // light green	TMS9918
+		{ 0x6d, 0xff, 0x6d, 0xff,  3 }, // light green			V9938
+		{ 0x54, 0x4e, 0xff, 0xff,  4 }, // dark blue	TMS9918
+		{ 0x24, 0x24, 0xff, 0xff,  4 }, // dark blue			V9938
+		{ 0x7d, 0x70, 0xff, 0xff,  5 }, // light blue	TMS9918
+		{ 0x48, 0x6d, 0xff, 0xff,  5 }, // light blue			V9938
+		{ 0xd2, 0x54, 0x42, 0xff,  6 }, // dark red		TMS9918
+		{ 0xb6, 0x24, 0x24, 0xff,  6 }, // dark red				V9938
+		{ 0x45, 0xe8, 0xff, 0xff,  7 }, // cyan			TMS9918
+		{ 0x48, 0xda, 0xff, 0xff,  7 }, // cyan					V9938
+		{ 0xfa, 0x59, 0x48, 0xff,  8 }, // medium red	TMS9918
+		{ 0xff, 0x24, 0x24, 0xff,  8 }, // medium red			V9938
+		{ 0xff, 0x7c, 0x6c, 0xff,  9 }, // light red	TMS9918
+		{ 0xff, 0x6d, 0x6d, 0xff,  9 }, // light red			V9938
+		{ 0xd3, 0xc6, 0x3c, 0xff, 10 }, // dark yellow	TMS9918
+		{ 0xda, 0xda, 0x24, 0xff, 10 }, // dark yellow			V9938
+		{ 0xe5, 0xd2, 0x6d, 0xff, 11 }, // light yellow	TMS9918
+		{ 0xda, 0xda, 0x91, 0xff, 11 }, // light yellow			V9938
+		{ 0x23, 0xb2, 0x2c, 0xff, 12 }, // dark green	TMS9918
+		{ 0x24, 0x91, 0x24, 0xff, 12 }, // dark green			V9938
+		{ 0xc8, 0x5a, 0xc6, 0xff, 13 }, // magenta		TMS9918
+		{ 0xda, 0x48, 0xb6, 0xff, 13 }, // magenta				V9938
+		{ 0xcc, 0xcc, 0xcc, 0xff, 14 }, // gray			TMS9918
+		{ 0xb6, 0xb6, 0xb6, 0xff, 14 }, // gray					V9938
+		{ 0xff, 0xff, 0xff, 0xff, 15 }, // white		TMS9918	V9938
 
-		{ 0x00, 0x00, 0x00, 0x00,  1 }, // black		TMS9918	V9938
-		{ 0x23, 0xcb, 0x32, 0x00,  2 }, // medium green	TMS9918
-		{ 0x24, 0xda, 0x24, 0x00,  2 }, // medium green			V9938
-		{ 0x60, 0xdd, 0x6c, 0x00,  3 }, // light green	TMS9918
-		{ 0x6d, 0xff, 0x6d, 0x00,  3 }, // light green			V9938
-		{ 0x54, 0x4e, 0xff, 0x00,  4 }, // dark blue	TMS9918
-		{ 0x24, 0x24, 0xff, 0x00,  4 }, // dark blue			V9938
-		{ 0x7d, 0x70, 0xff, 0x00,  5 }, // light blue	TMS9918
-		{ 0x48, 0x6d, 0xff, 0x00,  5 }, // light blue			V9938
-		{ 0xd2, 0x54, 0x42, 0x00,  6 }, // dark red		TMS9918
-		{ 0xb6, 0x24, 0x24, 0x00,  6 }, // dark red				V9938
-		{ 0x45, 0xe8, 0xff, 0x00,  7 }, // cyan			TMS9918
-		{ 0x48, 0xda, 0xff, 0x00,  7 }, // cyan					V9938
-		{ 0xfa, 0x59, 0x48, 0x00,  8 }, // medium red	TMS9918
-		{ 0xff, 0x24, 0x24, 0x00,  8 }, // medium red			V9938
-		{ 0xff, 0x7c, 0x6c, 0x00,  9 }, // light red	TMS9918
-		{ 0xff, 0x6d, 0x6d, 0x00,  9 }, // light red			V9938
-		{ 0xd3, 0xc6, 0x3c, 0x00, 10 }, // dark yellow	TMS9918
-		{ 0xda, 0xda, 0x24, 0x00, 10 }, // dark yellow			V9938
-		{ 0xe5, 0xd2, 0x6d, 0x00, 11 }, // light yellow	TMS9918
-		{ 0xda, 0xda, 0x91, 0x00, 11 }, // light yellow			V9938
-		{ 0x23, 0xb2, 0x2c, 0x00, 12 }, // dark green	TMS9918
-		{ 0x24, 0x91, 0x24, 0x00, 12 }, // dark green			V9938
-		{ 0xc8, 0x5a, 0xc6, 0x00, 13 }, // magenta		TMS9918
-		{ 0xda, 0x48, 0xb6, 0x00, 13 }, // magenta				V9938
-		{ 0xcc, 0xcc, 0xcc, 0x00, 14 }, // gray			TMS9918
-		{ 0xb6, 0xb6, 0xb6, 0x00, 14 }, // gray					V9938
-		{ 0xff, 0xff, 0xff, 0x00, 15 }  // white		TMS9918	V9938
+		{ 0x40, 0x40, 0x40, 0xff,  0 }, // dark grey as transparent
+		{ 0x00, 0x00, 0x00, 0x00,  0 }  // actually transparent
 	};
 
 /* Private function prototypes --------------------------------------------- */
@@ -109,7 +109,11 @@ int pngReaderRead(char *pngFilename, struct stBitmap *bitmap) {
 	byte *source, *target;
 	for (source = pngImage, target = bitmap->bitmap, y = 0; y < bitmap->height; y++) {
 		for (x = 0; x < bitmap->width; x++) {
-			*(target++) = paletteIndex(*(source++), *(source++), *(source++), *(source++));
+			byte r = *(source++);
+			byte g = *(source++);
+			byte b = *(source++);
+			byte a = *(source++);
+			*(target++) = paletteIndex(r, g, b, a);
 		}
 	}
 
@@ -127,7 +131,7 @@ byte paletteIndex(byte r, byte g, byte b, byte a) {
 	struct stColor *it;
 	struct stColor *closestColor = 0;
 	int minDistance = 255*4;
-	for (i = 0, n = 30, it = palette; i < n; i++) {
+	for (i = 0, n = 30, it = palette; i < n; i++, it++) {
 		int dist = distance(it, r, g, b, a);
 		if (dist == 0) {
 			return it->index;
@@ -142,12 +146,13 @@ byte paletteIndex(byte r, byte g, byte b, byte a) {
 
 int distance(struct stColor *color, byte r, byte g, byte b, byte a) {
 
-	if ((color->a == 0xff) && (a == 0xff)) {
+	if ((color->a == 0x00) && (a == 0x00)) {
 		return 0;
 	}
 
 	return    abs((int) color->r - (int) r)
 			+ abs((int) color->g - (int) g)
-			+ abs((int) color->b - (int) b);
+			+ abs((int) color->b - (int) b)
+			+ abs((int) color->a - (int) a);
 }
 
